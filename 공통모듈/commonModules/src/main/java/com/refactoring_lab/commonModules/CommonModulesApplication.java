d3c56418e2abb789fb6694dd2779c3e78ed823e7
jwkim <wer07032@naver.com>
2024-10-16 16:37:03 +0900

    #1-common-modules Init

---
+++ 공통모듈/commonModules/src/main/java/com/refactoring_lab/commonModules/CommonModulesApplication.java
@@ -0,0 +1,13 @@
+package com.refactoring_lab.commonModules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonModulesApplication.class, args);
+	}
+
+}
